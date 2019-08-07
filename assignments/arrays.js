47a5606722f8f6829ebbaca03e933fee7e92a733
Westley Feller <feller.westley@gmail.com>
2019-08-06 21:59:22 -0700

    Completed arrays objective 6

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -197,6 +197,19 @@ let inventoryFilter = (inventory_data) => {
                 }
             }
             return oldCars;
+
+        case "bmw_audi":
+            let BMWAndAudi = [];
+            for (let i = 0; i < inventory.length; i++) {
+                if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
+                    BMWAndAudi.push(inventory[i]);
+                }
+            }
+            return BMWAndAudi;
+
+        default:
+            break;
+
     }
 };
 
@@ -214,8 +227,8 @@ console.log(inventoryFilter("old_cars").length);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = [];
-console.log();
+
+console.log(JSON.stringify(inventoryFilter("bmw_audi")));
 
 
 
